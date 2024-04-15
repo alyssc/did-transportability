427d2cee12c78f08d7753c2ce50e6adb7c5570e1
laura-hatfield <hatfield@hcp.med.harvard.edu>
2024-04-15 07:17:19 -0400

    calls plotting functions, pulls results for reporting in the text

--- HSR_plot_scaled.R
+++ HSR_plot_scaled.R
@@ -6,16 +6,21 @@ load("2024-04-13_HSR_results.RData")
 scenario.order <- (output$sumstats$diff.by.AS.long %>% filter(name=="Non-SSP, system") %>%
   group_by(scenario) %>% summarize(median=quantile(diff,p=0.5)) %>% arrange(median))$scenario
 # Find the new number of the base scenario:
-which(scenario.order==1)
 
 datplots(output$sumstats,save.figs=T,prefix="scenario",order=scenario.order)
 estplots(output$ests,output$sumstats,save.figs=T,prefix="scenario",order=scenario.order)
+calibplots(output$sumstats,save.figs=T,which.base=which(scenario.order==1))
 
 # For reporting in the text:
 output$sumstats$by.AS.long %>% mutate(scenario=factor(scenario,levels=scenario.order,labels=1:length(scenario.order))) %>%
-  filter(name=="Non-SSP, system") %>%
+  filter(name=="Non-SSP, system",trt=="Treated") %>%
   group_by(scenario,group) %>% summarize(median=quantile(value,p=0.5)) %>%
   filter(scenario%in%c(1,16))
+
+output$sumstats$diff.by.AS.long %>% 
+  mutate(scenario=factor(scenario,levels=scenario.order,labels=1:length(scenario.order))) %>%
+  filter(scenario==9) %>%
+  group_by(name) %>% summarize(median=quantile(diff,p=0.5))
 
 output$ests %>% mutate(scenario=factor(scenario,levels=scenario.order,labels=1:length(scenario.order))) %>%
   group_by(scenario) %>% 
@@ -23,4 +28,40 @@ output$ests %>% mutate(scenario=factor(scenario,levels=scenario.order,labels=1:l
             est.diff=quantile(est.diff,0.5),
             true.patt=quantile(true.patt,0.5),
             true.satt=quantile(true.satt,0.5)) %>%
-  filter(scenario%in%c(1,16))
+  filter(scenario%in%c(1,9,16))
+
+scenario_params <- data.frame(expand.grid(
+  x1.r = -.617, 
+  x2.r = -.715,
+  phi.1= c(-.18,.18),  # log odds ratio of Pr(SSP) in CPC+ vs non-CPC+
+  phi.2=c(-.06,.06),   # log odds ratio of Pr(system) in CPC+ vs non-CPC+
+  
+  q= -1.28,
+  om.1 = c(-1,1),  # log odds ratio of Pr(Black) in SSP vs non-SSP
+  om.2 = c(-1,1), # log odds ratio of Pr(Black) in system vs indep
+  om.3 = -.235,
+  
+  H = 0, sigma.H = 0.02,
+  psi.1 = -.03, 
+  
+  theta.P = -68.5, sigma.P = 69, 
+  gamma.3 = -92, 
+  gamma.4 = 206, 
+  gamma.5 = -87,
+  
+  beta.0 = -3.05,
+  beta.3 = 0,
+  beta.4 = .839,
+  beta.5 = 1.17,
+  beta.6 = .778, 
+  
+  alpha.0 = 10100,
+  alpha.1 = 46500, 
+  alpha.2 = 600,
+  alpha.3 = 12,
+  
+  P= 1200)) %>%
+  mutate(scenario=c(1:16)) %>%
+  mutate(scenario=factor(scenario,levels=scenario.order,labels=1:length(scenario.order)))
+
+write_csv(scenario_params %>% select(scenario,om.1,om.2,phi.1,phi.2),"scenario_params_varying.csv")
