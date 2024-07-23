fc814a65c6dc6f020504c1ab2c83c41964ff2b59
Alyssa Chen <alyssachen@college.harvard.edu>
2024-07-23 11:59:22 -0400

    adding result plots

--- HSR_plot_scaled.R
+++ HSR_plot_scaled.R
@@ -12,13 +12,18 @@ scenario.order <- (scenario.output$sumstats$diff.by.AS.long %>% filter(name=="No
 
 viol_10.scenario.order <- c(1:9)
 
-outputs <- c(scenario.output, viol_10_output)
-scenario.orders <- c(scenario.order, viol_10.scenario.order)
 for(i in c(1:2)){
-  output <- outputs[i]
-  order <- scenario.orders[i]
-  datplots(output$sumstats,save.figs=T,prefix="plots/scenario",order=order)
-  estplots(output$ests,output$sumstats,save.figs=T,prefix="plots/scenario",order=order)
+  if(i==1){
+    output <- scenario.output
+    prefix <- "plots/scenario"
+    order <- scenario.order
+  }else{
+    output <- viol_10_output
+    prefix <- "plots/viol_10"
+    order <- viol_10.scenario.order
+  }
+  datplots(output$sumstats,save.figs=T,prefix=prefix,order=order)
+  estplots(output$ests,output$sumstats,save.figs=T,prefix=prefix,order=order)
 }
 
 calibplots(scenario.output$sumstats,save.figs=T,which.base=which(order==1))
@@ -78,3 +83,43 @@ scenario_params <- data.frame(expand.grid(
   mutate(scenario=factor(scenario,levels=scenario.order,labels=1:length(scenario.order)))
 
 write_csv(scenario_params %>% select(scenario,om.1,om.2,phi.1,phi.2),"scenario_params_varying.csv")
+
+
+
+viol_10_params <- data.frame(expand.grid(
+  x1.r = -.617, 
+  x2.r = -.715,
+  phi.1= -0.188,  # log odds ratio of Pr(SSP) in CPC+ vs non-CPC+
+  phi.2= -0.057,   # log odds ratio of Pr(system) in CPC+ vs non-CPC+
+  
+  q= -1.38,
+  om.1 = -1.25,  # log odds ratio of Pr(Black) in SSP vs non-SSP
+  om.2 = -1, # log odds ratio of Pr(Black) in system vs indep
+  om.3 = -0.1389,
+  
+  H = 0, sigma.H = 0.02,
+  psi.1 = -.03, 
+  
+  theta.P = -68.5, sigma.P = 69, 
+  gamma.3 = -92, 
+  gamma.4 = 206, 
+  gamma.5 = -87,
+  gamma.6=c(0,50,200),
+  gamma.7=c(0,50,200),
+  
+  beta.0 = -3.063,
+  beta.3 = 0,
+  beta.4 = 1.02,
+  beta.5 = 1.301,
+  beta.6 = 0.6076, 
+  
+  alpha.0 = 10100,
+  alpha.1 = 46500, 
+  alpha.2 = 600,
+  alpha.3 = 12,
+  
+  P= 1200)) %>%
+  mutate(scenario=c(1:9)) %>%
+  mutate(scenario=factor(scenario,levels=viol_10.scenario.order,labels=1:length(viol_10.scenario.order)))
+
+write_csv(viol_10_params %>% select(scenario,gamma.6,gamma.7),"viol_10_params_varying.csv")
